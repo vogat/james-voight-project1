61d9b15dc4e6dbdd4c9172b9cdee7c00c5ab5e0a
vogat <james.voight@me.com>
2023-06-05 14:26:13 -0400

    HTML & CSS basic layout

---
+++ main.js
@@ -0,0 +1,7 @@
+SYMBOL_LOOKUP = {
+    svn: {img: 'https://images2.imgbox.com/2e/2b/ParwUXPv_o.png'},
+    c: {img: 'https://images2.imgbox.com/ce/fd/iWb1vUJi_o.png'},
+    o: {img: 'https://images2.imgbox.com/b8/a2/xmr6TkYG_o.png'},
+    w: {img: 'https://images2.imgbox.com/b2/cd/JlGesziM_o.png'},
+    g: {img: 'https://images2.imgbox.com/ff/72/JhPrhfda_o.png'}
+}
